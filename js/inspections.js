0f6752668103a5ced703cd6de98e7e17a2e0df4d
dyardy <dyardy@gmail.com>
2013-10-10 19:50:08 -0400

    ddd

--- js/inspections.js
+++ js/inspections.js
@@ -145,7 +145,19 @@
     };
 
 
+    inspections.vm.sampleImage = function (item) {
 
+        var imageData = 'TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvbmx5IGJ5IGhpcyByZWFzb24sIGJ1dCBieSB0aGlzIHNpbmd1bGFyIHBhc3Npb24gZnJvbSBvdGhlciBhbmltYWxzLCB3aGljaCBpcyBhIGx1c3Qgb2YgdGhlIG1pbmQsIHRoYXQgYnkgYSBwZXJzZXZlcmFuY2Ugb2YgZGVsaWdodCBpbiB0aGUgY29udGludWVkIGFuZCBpbmRlZmF0aWdhYmxlIGdlbmVyYXRpb24gb2Yga25vd2xlZGdlLCBleGNlZWRzIHRoZSBzaG9ydCB2ZWhlbWVuY2Ugb2YgYW55IGNhcm5hbCBwbGVhc3VyZS4=';
+
+        var newImg = {
+            "ImageId": -1,
+            "InspectionId": item.InspectionId(),
+            "CameraImage": imageData
+        };
+        item.Modified(true);
+        item.InspectionImages.push(newImg);
+
+    };
 
 
     inspections.vm.capturePhoto = function () {
